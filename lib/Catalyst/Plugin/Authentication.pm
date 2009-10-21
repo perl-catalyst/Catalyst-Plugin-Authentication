11de9302ef2bccd68799ad9ed48e0ec0ad9b67be
Jonathan Yu <frequency@cpan.org>
2009-10-21 01:41:14 +0000

    Bring copyright statement in sync with the other packages

--- lib/Catalyst/Plugin/Authentication.pm
+++ lib/Catalyst/Plugin/Authentication.pm
@@ -1119,9 +1119,12 @@ Nigel Metheringham
 
 =head1 COPYRIGHT & LICENSE
 
-        Copyright (c) 2005 the aforementioned authors. All rights
-        reserved. This program is free software; you can redistribute
-        it and/or modify it under the same terms as Perl itself.
+Copyright (c) 2005 - 2009
+the Catalyst::Plugin::Authentication L</AUTHORS>
+as listed above.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
 
 =cut
 
