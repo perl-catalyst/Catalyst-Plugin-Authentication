a10b2f95856bedebe6dbcb663ba09f0ead09a083
Andrew Rodland <arodland@cpan.org>
2011-01-24 23:28:27 +0000

    Patch the live-app tests to work with PSGI-branch Catalyst.

--- t/lib/RemoteTestEngine.pm
+++ t/lib/RemoteTestEngine.pm
@@ -1,12 +1,27 @@
 package RemoteTestEngine;
-use base 'Catalyst::Engine::CGI';
+BEGIN {
+  require Catalyst;
+  if ($Catalyst::VERSION >= 5.89000) {
+    require Catalyst::Engine;
+    @ISA = qw(Catalyst::Engine);
+  } else {
+    require Catalyst::Engine::CGI;
+    @ISA = qw(Catalyst::Engine::CGI);
+  }
+}
 
 our $REMOTE_USER;
 our $SSL_CLIENT_S_DN;
 
 sub env {
     my $self = shift;
-    my %e = %ENV;
+    my %e;
+    if ($Catalyst::VERSION >= 5.89000) {
+      %e = %{ $self->SUPER::env() };
+    } else {
+      %e = %ENV;
+    }
+
     $e{REMOTE_USER} = $REMOTE_USER;
     $e{SSL_CLIENT_S_DN} = $SSL_CLIENT_S_DN;
     return \%e;    
