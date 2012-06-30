8462436bfc2bc8197cc4823b314630103d2bcd27
Tomas Doran <bobtfish@bobtfish.net>
2012-06-30 11:58:07 +0100

    Whitespace

--- lib/Catalyst/Authentication/Realm/Compatibility.pm
+++ lib/Catalyst/Authentication/Realm/Compatibility.pm
@@ -6,17 +6,17 @@ use warnings;
 use base qw/Catalyst::Authentication::Realm/;
 
 ## very funky - the problem here is that we can't do real realm initialization
-## but we need a real realm object to function.  So - we kinda fake it - we 
-## create an empty object - 
+## but we need a real realm object to function.  So - we kinda fake it - we
+## create an empty object -
 sub new {
     my ($class, $realmname, $config, $app) = @_;
-    
+
     my $self = { config => $config };
     bless $self, $class;
-    
+
     $self->config->{'use_session'} = $app->config->{'Plugin::Authentication'}{'use_session'} || '1';
     $self->name($realmname);
-    
+
     return $self;
 }
 
